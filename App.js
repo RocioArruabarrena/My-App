d1e44631eb6ca4511f5dab02d2b90ae6c04e8c01
ArruabarrenaRocio <rocioyedro07@gmail.com>
2025-10-01 20:11:02 -0300

    .

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import MoviesScreen from "./screens/MoviesScreen"
+import MovieScreen from "./screens/MoviesScreen"
 import MovieDetailScreen from "./screens/MovieDetailScreen"
 
 const Stack = createNativeStackNavigator()
